e571cfd4ae2461c590d52e22ebac74b2d14fba95
Mark Nielsen <nielsen.markus11@gmail.com>
2019-03-13 22:51:15 -0600

    Create minimal documentation and allow ability to pass app data.

--- R/ncaa-data.r
+++ R/ncaa-data.r
@@ -1,3 +1,10 @@
+#' @title Create a skeleton csv to manually input teams
+#' 
+#' @examples 
+#' # Prep file to input teams manually
+#' preteams <- team_skeleton()
+#' 
+#' @export
 team_skeleton <- function(){
   # Create skeleton
   preteams <- data.frame(rank=rep(1:16,4),
@@ -5,20 +12,33 @@ team_skeleton <- function(){
                          team=NA)
   
   write.csv(preteams, "preteams.csv",row.names = FALSE, na = "")
+  message("\"preteams.csv\" has been saved to your current working directory. Please manually enter the team names and upload using the input_teams() function.")
   return(preteams)
 }
 
-
-
-# Upload Teams
-input_teams <- function(file_path=system.file("extdata", "ncaa-teams.csv", package = "NCAAcalcutta", mustWork = TRUE)){
+#' @title Import csv of NCAA teams
+#' 
+#' @examples 
+#' # Input the 2018 teams
+#' teams <- import_teams(system.file("extdata", "ncaa-teams.csv", package = "NCAAcalcutta"))
+#' 
+#' @export
+import_teams <- function(file_path=system.file("extdata", "ncaa-teams.csv", package = "NCAAcalcutta", mustWork = TRUE)){
+  # Upload Teams
   df <- read.csv(file_path, header = TRUE)
   return(df)
 }
 
-
-# Randomize Teams
+#' @title Randomize order of teams for Calcutta auction
+#' 
+#' @examples 
+#' # Input the 2018 teams
+#' teams <- import_teams(system.file("extdata", "ncaa-teams.csv", package = "NCAAcalcutta"))
+#' teams <- randomize_teams(teams, random_seed=123)
+#' 
+#' @export
 randomize_teams <- function(df,random_seed = 156){
+  # Randomize Teams
   set.seed(random_seed)
   teams <- df
   rand1 <- runif(64)
---
+++ R/shiny-app.R
@@ -0,0 +1,163 @@
+#' @title Start the NCAA Calcutta Shiny App
+#' 
+#' @examples 
+#' # Input the 2018 teams
+#' teams <- import_teams(system.file("extdata", "ncaa-teams.csv", package = "NCAAcalcutta"))
+#' start_auction(teams, randomize=TRUE)
+#' 
+#' @export
+start_auction <- function(teams, randomize=TRUE){
+  require('shiny')
+  require('iterators')
+  
+  if (randomize) {
+    teams <- randomize_teams(teams)
+  }
+  
+  i <- 1
+  
+  teams.out <- NULL
+  shinyApp(
+    # Define UI for application that draws a histogram
+    ui = fluidPage(
+      
+      # Application title
+      titlePanel("NCAA Calcutta Competition"),
+      
+      # Sidebar with a slider input for number of bins 
+      wellPanel(
+        fluidRow(
+          column(4,
+                 textInput("owner","Bid Winner:"),
+                 textInput("cost","Cost:"),
+                 actionButton("submit","Submit")),
+          column(4,
+                 h2(htmlOutput("team")),
+                 h3(textOutput("minbid")),
+                 h4(textOutput("rank")),
+                 h4(textOutput("region"))),
+          column(4,
+                 h2(htmlOutput("timer")),
+                 actionButton("nextteam","Next Team"),
+                 actionButton("lastteam","Back")
+          )
+        )
+      ),
+      
+      # Show a plot of the generated distribution
+      fluidRow(
+        downloadButton('download',"Download the data"),
+        dataTableOutput("table")
+      )
+    )
+    ,
+    # Define server logic required to draw a histogram
+    server = function(input, output, session) {
+      
+      # curr.row <- reactive({
+      #   input$nextteam
+      #   
+      #   nextElem(iteams)
+      # })
+      
+      observe({
+        input$nextteam
+        i <<- i+1
+      })
+      
+      observe({
+        input$lastteam
+        i <<- i-1
+      })
+      
+      last.time <- reactive({
+        input$nextteam
+        input$lastteam
+        # curr.row()
+        
+        secs <- ifelse(teams$rank[i]<=2,120,
+                       ifelse(teams$rank[i]<=4,90,
+                              ifelse(teams$rank[i]<=12,60,45)
+                       )
+        )
+        
+        eventTime <- Sys.time() + secs
+      })
+      
+      output$team <- renderText({
+        input$nextteam
+        input$lastteam
+        # tmp <- curr.row()
+        paste0("Current Team: <font color=\"#FF0000\">",
+               teams$team[i],
+               "</font>"
+               # as.character(tmp$team)
+        )
+      })
+      
+      output$minbid <- renderText({
+        input$nextteam
+        input$lastteam
+        minbid <- ifelse(teams$rank[i]<=4,100,
+                         ifelse(teams$rank[i]<=10,55,35)
+        )
+        
+        paste0("Minimum Bid: ",minbid)
+      })
+      
+      output$rank <- renderText({
+        input$nextteam
+        input$lastteam
+        # tmp <- curr.row()
+        paste0("Rank: ",
+               teams$rank[i]
+               # as.character(tmp$rank)
+        )
+      })
+      
+      output$region <- renderText({
+        input$nextteam
+        input$lastteam
+        # tmp <- curr.row()
+        paste0("Region: ",
+               teams$region[i]
+               # as.character(tmp$region)
+        )
+      })
+      
+      output$timer <- renderText({
+        eventTime <- last.time()
+        invalidateLater(1000,session)
+        paste0("Time Remaining: <font color=\"#0000FF\">", 
+               round(difftime(eventTime, Sys.time(),units = 'secs')),
+               '</font> seconds'
+        )
+      })
+      
+      
+      thedata <- reactive({
+        input$submit
+        owner <- isolate(input$owner)
+        cost <- isolate(input$cost)
+        
+        tmp.dat <- data.frame(teams[i,])
+        tmp.dat$owner <- owner
+        tmp.dat$bid <- cost
+        teams.out <<- rbind(teams.out,tmp.dat)
+      })
+      
+      output$table <- renderDataTable({
+        thedata()
+      },options = list(searching=F, paging = F, sort = F))
+      
+      output$download <- downloadHandler(
+        filename = function(){"teams-out.csv"},
+        content = function(fname){
+          write.csv(thedata(), fname)
+        }
+      )
+    }
+  )
+  
+  
+}
